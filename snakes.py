330e82d5bd295a91a8e6c96fc0b24650a7f208ac
Miles Erickson <miles.erickson@gmail.com>
2019-04-23 16:08:29 -0700

    Remove extra docstring

--- snakes.py
+++ snakes.py
@@ -5,7 +5,6 @@ class Snake:
 
 class Cobra(Snake):
     """Definitely dangerous, yup."""
-    """KEVINS LOUD EDIT"""
     def bite(self, other):
         """Deliver a dose of venom."""
         pass
